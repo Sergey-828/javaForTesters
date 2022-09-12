c5e84d4e77ec1a86520e0e085bb1b195af751dd9
SergeyK <98507907+Sergey-828@users.noreply.github.com>
2022-09-12 13:50:48 -0700

    TestJava_01

--- source/src/test/java/com/javafortesters/chap003myfirsttest/examples/interim/emptyMethodTestAnnotated/MyFirstTest.java
+++ source/src/test/java/com/javafortesters/chap003myfirsttest/examples/interim/emptyMethodTestAnnotated/MyFirstTest.java
@@ -3,9 +3,14 @@ package com.javafortesters.chap003myfirsttest.examples.interim.emptyMethodTestAn
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class MyFirstTest {
 
     @Test
     public void canAddTwoPlusTwo(){
+        int answer = 2 + 2;
+        assertEquals("2+2=4",4,answer);
+        System.out.println(answer);
     }
 }
